4fe9657dcf6eb1bd395d83892e36aeaceba7e7fe
alenleban <alen007leban@gmail.com>
2022-06-30 13:19:24 +0200

    Dodal fakulteto

--- program.c
+++ program.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int fakulteta(int i);
+
 int main() {
 	
+	//printf("%d", fakulteta(5));
 	return 0;
+}
+
+int fakulteta(int j)
+{
+	int rez = 1;
+	for (int i = j; i > 0; i--)
+	{
+		rez *= i;
+	}
+	return rez;
 }
