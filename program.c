150e2a8bf0c3f8cfc5516a112f459124455e7fc7
anzecrv <anze.crv@gmail.com>
2022-06-30 13:25:29 +0200

    added input feature

--- program.c
+++ program.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int vrniStevilo(int i) {
+	
+	return i;
+}
+
 int main() {
+	int stevilo;
+	printf("Napisi stevilo: ");
+	scanf("%d", &stevilo);
+	
+	int fak;	//spremenljivka za fakulteto stevila 
+	
+	printf("tvoje stevilo je %d\n", vrniStevilo(stevilo));
+	printf("fakulteta tvojega stevila je %d\n", fak);
 	
 	return 0;
 }
